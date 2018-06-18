e9dcb9df2d402a7bcd0bd846481448e046f81608
Aidy Makhataev <makataev.7@gmail.com>
2018-06-18 20:05:50 +0600

    setting up sequelize on server, add vuetify on front

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,41 +1,72 @@
 <template>
-    <div>
-        <h1>Register page!</h1>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-toolbar>
+            <v-toolbar-title>Register Page!</v-toolbar-title>
+        </v-toolbar>
 
-        <input 
-            type="email" 
-            name="email" 
-            placeholder="email" 
-            v-model="email">
-        <br>
-        <input 
-            type="password" 
-            name="password" 
-            placeholder="password" 
-            v-model="password">
-        <br>
-        <button @click="register">Register</button>
-    </div>
+        <v-card-text justify-center>
+            <v-form v-model="valid" ref="form" lazy-validation>
+                <v-text-field
+                    label="Email"
+                    v-model="email"
+                    required
+                >
+                </v-text-field>
+                <v-text-field
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                    required
+                >
+                </v-text-field>
+                
+                <v-btn
+                    :disabled="!valid"
+                    @click="register"
+                > 
+                    register
+                </v-btn>
+
+                <v-btn @click="clear">clear</v-btn>
+           
+            </v-form>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
-
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
+
 export default {
-    data () {
-        return {
-            email: 'abc',
-            password: '123'
-        }
-    },
-    methods: {
-        async register() {
-            response = await AuthenticationService.register({
-                email: this.email,
-                password: this.password
-            })
-        }
+  data () {
+    return {
+      valid: true,
+      email: '',
+      password: '',
+      error: null
     }
+  },
+  methods: {
+    async register () {
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    },
+    clear () {
+        this.$refs.form.reset()
+    }
+  }
 }
 </script>
 
+<style scoped>
+</style>
