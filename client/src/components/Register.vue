9d709d4f5a1e9fa36daf4777bad45ee0814b828c
Aidy Makhataev <makataev.7@gmail.com>
2018-06-18 20:23:38 +0600

    client: styling register component

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -11,6 +11,7 @@
                 <v-text-field
                     label="Email"
                     v-model="email"
+                    :rules="emailRules"
                     required
                 >
                 </v-text-field>
@@ -18,12 +19,17 @@
                     label="Password"
                     type="password"
                     v-model="password"
+                    :rules="passwordRules"
                     required
                 >
                 </v-text-field>
-                
+                <br>
+                <div class="red" v-html="error" />
+                <br>
                 <v-btn
                     :disabled="!valid"
+                    dark
+                    class="cyan"
                     @click="register"
                 > 
                     register
@@ -47,19 +53,31 @@ export default {
       valid: true,
       email: '',
       password: '',
-      error: null
+      error: null,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'password is required',
+        v => (v && v.length <= 32) || 'password must be less than 32 characters',
+        v => (v && v.length >= 8) || 'password must be at least 8 characters in length',
+      ],
     }
   },
   methods: {
     async register () {
-      try {
-        const response = await AuthenticationService.register({
-          email: this.email,
-          password: this.password
-        })
-      } catch (error) {
-        this.error = error.response.data.error
-      }
+        if (this.$refs.form.validate()) {
+            try {
+                const response = await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+            } catch (error) {
+                this.error = error.response.data.error
+            }
+        }
+      
     },
     clear () {
         this.$refs.form.reset()
